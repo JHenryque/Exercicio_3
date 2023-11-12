1661f90d0c3d1e11ce851f9c54779c175303efd9
Henrique <henryque.pc.game@gmail.com>
2023-11-11 22:12:43 -0300

    atualizado

--- main.c
+++ main.c
@@ -1,9 +1,3 @@
-/*
-O programa deve solicitar ao usuário que insira um númenro inteiro não negativo.<br/>
-O programa deve verificar se o número inserido pelo usuário é não negativo, se nao for, o programa deve exibir uma mensagem de erro e pedir que o usuário insira um número valido.<br/>
-O programa deve calcular o fatorial do número inserido pelo usuário.<br/>
-O programa deve exibir o resltado.<br/>
-*/
 #include <stdio.h>
 #include <locale.h>
 
